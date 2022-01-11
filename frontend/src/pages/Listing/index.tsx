2fec1397e2a3bff953e0c46a34f0a82e9c4613e9
graciele.oliveira <oliveiragracie@gmail.com>
2022-01-11 12:10:37 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,38 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
-function Listing(){
+function Listing() {
 
     return (
-        <Pagination />
-    )
+        <>
+            <Pagination />
 
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Listing;
+
+
+/** <> </> = fragment */
+/** col-lg-4 = a partir da largura 992px cada card ocupa 4 das 12 colunas (padrão) da tela ou seja serão 3 cards por coluna */
+/** mb margin  botton */
