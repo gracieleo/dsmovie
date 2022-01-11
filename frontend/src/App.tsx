099a123fde6581ec99c7ad0204d4146c994afc7d
graciele.oliveira <oliveiragracie@gmail.com>
2022-01-11 09:34:14 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
-import Navbar from "./components/Navbar";
+/*Configuração das rotas,páginas*/
 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';    /** considerando 2 páginas */
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
+
+/**rota padrão: http://localhost:3000/ */
+/**rota formulário: http://localhost:3000/form/1 */
 
 function App() {
   return (
-    
-    <Navbar />
+    <BrowserRouter>   
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} /> 
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
